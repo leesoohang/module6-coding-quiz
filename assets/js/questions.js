155155d4a176ee9da6bebbe7796f87154a18e690
Soo <li_xiuheng@hotmail.com>
2023-12-31 00:23:52 +0000

    first draft

---
+++ assets/js/questions.js
@@ -0,0 +1,287 @@
+//quiz questions and answers stored as objects
+var questions = [ 
+{prompt: "1. Given the array below, how would you access the item whose value is 7?\n var numbers=[4, 7, 3, 5, 2];",
+choices: 
+["a)	numbers(2)",
+"b)	numbers[1]",
+"c)	numbers[2]",
+"d)	numbers(7)"],
+answer: "b)	numbers[1]",
+}
+,
+{prompt: "2.	What is logged in the console after this code is executed?\nlet sum = 23.95;\nlet tip = '3';\nconsole.log('The total is $' + sum + tip + '.');",
+choices: 
+["a)	Syntax error.",
+"b)	'The total is $26.95 + 3'",
+"c)	'The total is $26.95.'",
+"d)	'The total is $23.953.'"
+],
+answer: "d)	'The total is $23.953.'",
+}
+,
+{prompt: "3.	What does a single equals symbol in JavaScript indicate?",
+choices: 
+["a)	The values to the left and right are compared.",
+    "b)	A value is matched with the value of a named variable.",
+    "c)	The values to the left and right are equal.",
+    "d)	A value is assigned to the named variable."
+    
+],
+answer: "d)	A value is assigned to the named variable.",
+}
+,
+{prompt: "4.	What will be printed to the console as the following script is running?\nvar sqrt = (function(x) {\nconsole.log(x*x);\n})(my_number)\nmy_number = 5;",
+choices: 
+["a)	x*x",
+    "b)	25",
+    "c)	5",
+    "d)	This code will error out."
+],
+answer: "d)	This code will error out.",
+}
+,
+{prompt: "5.	In a <i>switch</i> statement, what happens when several of the switch cases resolve to True?",
+choices: 
+["a)	The <i>switch</i> statement returns all cases that resolves to <i>true</i> , then stops.",
+    "b)	The <i>switch</i> statement returns the last case that resolves to <i>true</i> , then stops.",
+    "c)	The <i>switch</i> statement returns the first case that resolves to <i>true</i> , then stops.",
+    "d)	The switch statement returns an error. Each switch case should be unique."
+    
+],
+answer: "c)	The <i>switch</i> statement returns the first case that resolves to <i>true</i> , then stops.",
+}
+,
+{prompt: "6.	What is the new value of myArray after this script executes?\nconst myArray = [1, 2, 3, 4]\nmyArray.forEach( (item, index) = > {\nmyArray[index] = ++item;\n});",
+choices: 
+["a)	[1 2,3,4,2,3,4,5]",
+"b)	[1,2,3,4]",
+"c)	Syntax error: index not defined.",
+"d)	[2,3,4,5]"
+],
+answer: "d)	[2,3,4,5]",
+}
+,
+{prompt: "7.	What is the difference between the array.forEach() and array.map() methods?",
+choices: 
+["a)	array.forEach() executes a provided callback function once for each item in the array. array.map() creates an object map of the original array to be placed in a new variable.",
+    "b)	array.forEach() executes a provided callback function once for each item in the array and returns it to the original array. array.map() creates a new array with the results of executing a provided callback function once foreach item in the original array.",
+    "c)	array.forEach() creates a new array for the original data and executes a provided callback function once for each item in the array. array.map() creates a new array with the results of executing a provided callback function once foreach item in the original array.",
+    "d)	array.forEach() executes a provided callback function once for each item in the array. array.map() creates a new array with the results of executing a provided callback function once for each item in the original array."
+    
+],
+answer: "d)	array.forEach() executes a provided callback function once for each item in the array. array.map() creates a new array with the results of executing a provided callback function once for each item in the original array.",
+}
+,
+{prompt: "8.	Where in the HTML document does the new element appear when you use the document.createElement() method?",
+choices: 
+["a)	Nowhere: The element is created, but has not been added to the DOM.",
+"b)	At the bottom of the HTML document.",
+"c)	Inside the specified element container.",
+"d)	At the top of the HTML document."
+
+],
+answer: "a)	Nowhere: The element is created, but has not been added to the DOM.",
+}
+,
+{prompt: "9.	What is the 'DOM'?",
+choices: 
+["a)	DOM is short for 'Document Object Model', the document object the browser creates when it renders an HTML document.",
+"b)	DOM is the JavaScript environment the browser creates for each HTML document.",
+"c)	DOM is the name of a 2012 album by German singer Joachim Witt.",
+"d)	DOM is short for 'Document Object Master', the original document the browser renders."
+
+],
+answer: "a)	DOM is short for 'Document Object Model', the document object the browser creates when it renders an HTML document.",
+}
+,
+{prompt: "10.	What goes in place of a and b in this standard event listener?\nelement.addEventListener('a','b');",
+choices: 
+["a)	a: the event name\nb: the function to call when the event fires",
+"b)	a: the actions to perform when the event fires\nb: the name of the event",
+"c)	a: the count of how many such event listeners are defined\nb: an array of functions to call when the event fires",
+"d)	a: the function to call when the event fires\nb: the object ID of the element"
+
+],
+answer: "a)	a: the event name\nb: the function to call when the event fires"
+}
+]
+
+//set up DOM variables
+var questionsEl = 
+    document.querySelector("#questions"); 
+var timerEl = 
+    document.querySelector("#time"); 
+var choicesEl = 
+    document.querySelector("#choices"); 
+var submitBtn = document.querySelector("#submit"); 
+var startBtn = 
+    document.querySelector("#start"); 
+var nameEl = 
+    document.querySelector("#name"); 
+var feedbackEl = document.querySelector( 
+    "#feedback"
+); 
+var reStartBtn = 
+    document.querySelector("#restart"); 
+
+
+var currentQuestionIndex = 0; 
+var time = 200 ; 
+var timerId; 
+
+function quizStart() { 
+    timerId = setInterval( 
+        clockTick, 
+        1000 
+    ); 
+    timerEl.textContent = time; 
+    var landingScreenEl = 
+        document.getElementById("start-screen"); 
+    landingScreenEl.setAttribute("class", "hide"); 
+    questionsEl.removeAttribute( "class"); 
+    getQuestion(); 
+} 
+
+
+function getQuestion() { 
+    let currentQuestion = 
+        questions[currentQuestionIndex]; 
+    let promptEl = 
+        document.getElementById("question-title"); 
+    promptEl.textContent = 
+        currentQuestion.prompt; 
+    choicesEl.innerHTML = ""; 
+    currentQuestion.choices.forEach( 
+        function (choice, i) { 
+            let choiceBtn = 
+                document.createElement( 
+                    "button"
+                ); 
+            choiceBtn.setAttribute( 
+                "value", 
+                choice 
+            ); 
+            choiceBtn.textContent = 
+                i + 1 + ". " + choice; 
+            choiceBtn.onclick = 
+                questionClick; 
+            choicesEl.appendChild( 
+                choiceBtn 
+            ); 
+        } 
+    ); 
+} 
+
+function questionClick() { 
+    if ( 
+        this.value !== 
+        questions[currentQuestionIndex] 
+            .answer 
+    ) { 
+        time -= 200; 
+        if (time < 0) { 
+            time = 0; 
+        } 
+        timerEl.textContent = time; 
+        feedbackEl.textContent = "Oops!"; 
+    } else { 
+        feedbackEl.textContent = 
+            "Correct!"; 
+    } 
+    feedbackEl.setAttribute( 
+        "class", 
+        "feedback"
+    ); 
+    setTimeout(function () { 
+        feedbackEl.setAttribute( 
+            "class", 
+            "feedback hide"
+        ); 
+    }, 2000); 
+    currentQuestionIndex++; 
+    if ( 
+        currentQuestionIndex === 
+        questions.length 
+    ) { 
+        quizEnd(); 
+    } else { 
+        getQuestion(); 
+    } 
+} 
+
+function quizEnd() { 
+    clearInterval(timerId); 
+    let endScreenEl = 
+        document.getElementById( 
+            "end-screen"
+        ); 
+    endScreenEl.removeAttribute( 
+        "class"
+    ); 
+    let finalScoreEl = 
+        document.getElementById( 
+            "final-score"
+        ); 
+    finalScoreEl.textContent = time; 
+    questionsEl.setAttribute( 
+        "class", 
+        "hide"
+    ); 
+} 
+  
+// End quiz if timer reaches 0 
+  
+function clockTick() { 
+    time--; 
+    timerEl.textContent = time; 
+    if (time <= 0) { 
+        quizEnd(); 
+    } 
+} 
+
+// Save score in local storage 
+// Along with users' name 
+  
+function saveHighscore() { 
+    let name = nameEl.value.trim(); 
+    if (name !== "") { 
+        let highscores = 
+            JSON.parse( 
+                window.localStorage.getItem( 
+                    "highscores"
+                ) 
+            ) || []; 
+        let newScore = { 
+            score: time, 
+            name: name, 
+        }; 
+        highscores.push(newScore); 
+        window.localStorage.setItem( 
+            "highscores", 
+            JSON.stringify(highscores) 
+        ); 
+        alert( 
+            "Your Score has been Submitted"
+        ); 
+    } 
+} 
+  
+// Save users' score after pressing enter 
+  
+function checkForEnter(event) { 
+    if (event.key === "Enter") { 
+        saveHighscore(); 
+        alert( 
+            "Your Score has been Submitted"
+        ); 
+    } 
+} 
+nameEl.onkeyup = checkForEnter; 
+  
+// Save users' score after clicking submit 
+  
+submitBtn.onclick = saveHighscore; 
+  
+// Start quiz after clicking start quiz 
+  
+startBtn.onclick = quizStart;
